f6784738b831c91b3cbf9c43001e660eb74c14fe
Braian-Quintian <bquintianromero@gmail.com>
2023-11-02 17:15:15 -0500

    feat: :sparkles: Se ha añadido el limit

---
+++ frontend/backend/src/config/limit.js
@@ -0,0 +1,37 @@
+import rateLimit from "express-rate-limit";
+
+export let limitLogin = () => {
+  return rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 3,
+    standardHeaders: true,
+    legacyHeaders: false,
+    skip: (req, res) => {
+      if (req.headers["content-length"] > 100) {
+        res.status(413).send({ message: "Tamaño de la solicitud alcanzado" });
+        return true;
+      }
+    },
+    message: (req, res) => {
+      res.status(429).send({ message: "Limite alcanzado" });
+    },
+  });
+};
+
+export let limitRegister = () => {
+  return rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 3,
+    standardHeaders: true,
+    legacyHeaders: false,
+    skip: (req, res) => {
+      if (req.headers["content-length"] > 100) {
+        res.status(413).send({ message: "Tamaño de la solicitud alcanzado" });
+        return true;
+      }
+    },
+    message: (req, res) => {
+      res.status(429).send({ message: "Limite alcanzado" });
+    },
+  });
+};
